c721656170f12091d7f866b044f92f41c0b5b3b9
Carolina Perez-Vargas <cpvp20@gmail.com>
2021-03-17 21:27:20 -0500

    function is now called inside post route

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
+require('dotenv');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const {
+    IamAuthenticator
+} = require('ibm-watson/auth');
 
 const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: '{apikey}',
-  }),
-  serviceUrl: '{url}',
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({
+        apikey: process.env.APIKEY,
+    }),
+    serviceUrl: process.env.URL,
 });
 
-const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
 
-const toneParams = {
-  toneInput: { 'text': text },
-  contentType: 'application/json',
-};
+app.get('/', function (req, res) {
+    res.send('Hello there');
+});
 
-toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => {
-    console.log(JSON.stringify(toneAnalysis, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
+app.post('/tone', (req, res) => {
+    const text = 'Team, I know that times are tough! Product ' +
+        'sales have been disappointing for the past three ' +
+        'quarters. We have a competitive product, but we ' +
+        'need to do a better job of selling it!';
+
+    const toneParams = {
+        toneInput: {
+            'text': text
+        },
+        contentType: 'application/json',
+    };
+
+    toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+            result = JSON.stringify(toneAnalysis, null, 2);
+            console.log(result);
+            res.json(result);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            res.send(err);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
